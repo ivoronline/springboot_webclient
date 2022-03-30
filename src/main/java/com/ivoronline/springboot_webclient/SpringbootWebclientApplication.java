5745bef9c5e36f48dd718f340ced87ec3870539e
igoreta <ivoronline@gmail.com>
2022-03-30 12:46:20 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_webclient/SpringbootWebclientApplication.java
+++ src/main/java/com/ivoronline/springboot_webclient/SpringbootWebclientApplication.java
@@ -1,6 +1,5 @@
 package com.ivoronline.springboot_webclient;
 
-import com.ivoronline.springboot_webclient.services.PersonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +8,6 @@ public class SpringbootWebclientApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringbootWebclientApplication.class, args);
-    PersonService.getPerson();
-    PersonService.getPersons();
   }
 
 }
--- src/main/java/com/ivoronline/springboot_webclient/services/PersonService.java
+++ src/main/java/com/ivoronline/springboot_webclient/services/PersonService.java
@@ -1,19 +1,39 @@
 package com.ivoronline.springboot_webclient.services;
 
 import com.ivoronline.springboot_webclient.entities.Person;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import java.time.Duration;
-import java.util.List;
 
+@Component
 public class PersonService {
+
+  //===============================================================
+  // GET TEXT
+  //===============================================================
+  public void getText() {
+
+    //GET PERSON FROM SERVER
+    String string = WebClient.create("http://localhost:8080")
+                             .get()
+                             .uri("/GetText?name=John")
+                             .retrieve()
+                             .bodyToMono(String.class)
+                             .block(Duration.ofSeconds(3));
+
+    //DISPLAY PERSON
+    System.out.println("\n getText()");
+    System.out.println(string);
+
+  }
 
   //===============================================================
   // GET PERSON
   //===============================================================
-  public static void getPerson() {
+  public void getPerson() {
 
     //GET PERSON FROM SERVER
-    Person person = WebClient.create("http://localhost:8085")
+    Person person = WebClient.create("http://localhost:8080")
                              .get()
                              .uri("/GetPerson")
                              .retrieve()
@@ -21,7 +41,7 @@ public class PersonService {
                              .block(Duration.ofSeconds(3));
 
     //DISPLAY PERSON
-    System.out.println("getPerson()");
+    System.out.println("\n getPerson()");
     System.out.println(person.id + " " + person.name+ " " + person.age);
 
   }
@@ -29,10 +49,10 @@ public class PersonService {
   //===============================================================
   // GET PERSONS
   //===============================================================
-  public static void getPersons() {
+  public void getPersons() {
 
     //GET PERSON FROM SERVER
-    Person[] persons = WebClient.create("http://localhost:8085")
+    Person[] persons = WebClient.create("http://localhost:8080")
                                 .get()
                                 .uri("/GetPersons")
                                 .retrieve()
@@ -40,7 +60,7 @@ public class PersonService {
                                 .block(Duration.ofSeconds(3));
 
     //DISPLAY PERSONS
-    System.out.println("getPersons()");
+    System.out.println("\n getPersons()");
     for (Person person : persons) {
       System.out.println(person.id + " " + person.name+ " " + person.age);
     }
